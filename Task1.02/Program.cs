1afa8f1cb9a8f2e0cf01d0b7ddd496ba08d88436
Palex068 <palex068@gmail.com>
2022-11-22 17:15:10 +0300

    Add Task1.02 Вывод максимального из двух чисел

--- Task1.02/Program.cs
+++ Task1.02/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.Clear();
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Clear();
 
 Console.Write("Введите 1-е число: ");
 int Number1 = Convert.ToInt32(Console.ReadLine());
@@ -7,8 +9,8 @@ int Number2 = Convert.ToInt32(Console.ReadLine());
 
 if (Number2 > Number1)
     Console.WriteLine($"Максимальное число равно: {Number2}\nМинимальное число равно: {Number1}");
-    
+/*    
 else
     Console.WriteLine($"Максимальное число равно: {Number1}\nМинимальное число равно: {Number2}");
-    
+*/    
 Console.WriteLine();
