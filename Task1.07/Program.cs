658484431d8f45ecaba4167f7834cc337ecb0063
Palex068 <palex068@gmail.com>
2022-11-22 16:03:43 +0300

    Change Task1.07
    Задача 7 Вывод последней цифры числа
    Решение через %

--- Task1.07/Program.cs
+++ Task1.07/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.Write("Введите трехзначное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+while (N < 100 || N > 999) // Проверим число, что оно трехзначное
+{
+    Console.Write("Вы ошиблись!\nВведите трехзначное число: ");
+    N = Convert.ToInt32(Console.ReadLine());
+}    
+Console.Write($"Последняя цифра: {N % 10}");
+
+Console.WriteLine();
