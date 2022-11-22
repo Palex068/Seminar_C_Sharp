e0d9cb196027ba290582c582335f558f1b607691
Palex068 <palex068@gmail.com>
2022-11-22 13:10:09 +0300

    Change Task1.01
    Описали Коньюнкцию и Дизъюнкцию

--- Task1.01/Program.cs
+++ Task1.01/Program.cs
@@ -7,12 +7,12 @@ int number1 = int.Parse(Console.ReadLine());
 
 // Console.Write("Введённое число больше 10? ");
 // Console.Write(Convert.ToInt32(number1 > 10));
-Console.Write("Введите второе число: ");
+// Console.Write("Введите второе число: ");
 // Способ ввода данных 2
-int number2 = Convert.ToInt32(Console.ReadLine());
+// int number2 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введённые числа не равны? ");
-Console.Write((number1 != number2));
+// Console.Write("Введённые числа не равны? ");
+// Console.Write((number1 != number2));
 // Console.WriteLine(number1 * 2);
 // Console.WriteLine(number2 * 3);
 
@@ -23,13 +23,16 @@ Console.Write((number1 != number2));
 // проверяет кратно ли оно одновременно 2 и 3, если ктарно,
 // то вывести Yes, инначе вывести No
 
-// Console.WriteLine();
-// Console.Write("Кратно ли введённое число одновременно 2 и 3 ? ");
-// if(number1 % 2 == 0 && number1 % 3 == 0)
-// {
-// Console.WriteLine("Yes");
-// }
-// else
-// {
-// Console.WriteLine("No");
-// }
+// Коньюнкция (логическое умножение) && матеиатическая запись ^
+// Дизъюнкция (логическое сложение) || матеиатическая запись v
+
+Console.WriteLine();
+Console.Write("Кратно ли введённое число одновременно 2 и 3 ? ");
+if(number1 % 2 == 0 && number1 % 3 == 0)
+{
+Console.WriteLine("Yes");
+}
+else
+{
+Console.WriteLine("No");
+}
