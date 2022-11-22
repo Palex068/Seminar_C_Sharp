a7457124d3109fa1033189331037ff98f38ef18a
Palex068 <palex068@gmail.com>
2022-11-22 17:08:02 +0300

    Add Task1.08 Вывод всех четных чисел от 1 до N

---
+++ Task1.08/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int I = 2;I <= N; I+=2)
+    Console.Write($"{I}\t");
+
+Console.WriteLine();
