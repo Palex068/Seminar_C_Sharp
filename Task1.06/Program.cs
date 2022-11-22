eb256e2133a25f8b1e48b2bc74e9f7670da5cb0e
Palex068 <palex068@gmail.com>
2022-11-22 16:50:06 +0300

    Add Task1.06
    Задача 6 Является ди введённое число четным

---
+++ Task1.06/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N % 2 == 0)
+    Console.WriteLine($"{N} кратно 2");
+else
+    Console.Write($"{N} не кратно 2");
+Console.WriteLine();
