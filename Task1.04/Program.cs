14c5392807482ab76c045fdfd516f808361a618d
Palex068 <palex068@gmail.com>
2022-11-22 16:43:00 +0300

    Add Task1.04
    Задача 4 Вывод максимального трех чисел

---
+++ Task1.04/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.Write("Введите 1-е число: ");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-е число: ");
+int Number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-е число: ");
+int Number3 = Convert.ToInt32(Console.ReadLine());
+
+int Max = Number1;
+
+if (Number2 > Max)
+    Max = Number2;
+    if(Number3 > Max)
+        Max = Number3;
+Console.WriteLine($"Максимальное число равно: {Max}");
+
+Console.WriteLine();
