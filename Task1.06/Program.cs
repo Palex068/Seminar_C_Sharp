bab4966051858de9e3462176fdb05d8b30cfe967
Palex068 <palex068@gmail.com>
2022-11-22 17:23:54 +0300

    Add Task1.06 Проверка на чётность/нечетность числа

--- Task1.06/Program.cs
+++ Task1.06/Program.cs
@@ -1,10 +1,12 @@
-﻿Console.Clear();
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Clear();
 
 Console.Write("Введите число: ");
 int N = Convert.ToInt32(Console.ReadLine());
 
 if (N % 2 == 0)
-    Console.WriteLine($"{N} кратно 2");
+    Console.WriteLine($"{N} чётное число");
 else
-    Console.Write($"{N} не кратно 2");
+    Console.Write($"{N} нечётное число");
 Console.WriteLine();
