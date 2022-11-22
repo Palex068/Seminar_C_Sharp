e0c1b3a423b4b2bf36bbdf3c3a7f98583784e2ff
Palex068 <palex068@gmail.com>
2022-11-22 15:03:07 +0300

    Change Task1.03
    Задача 3: Вывод названия дня недели
    Решил через цикл for

--- Task1.03/Program.cs
+++ Task1.03/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.Write("Введите порядковый номер дня недели: ");
+int numberDayOnWeek = Convert.ToInt32(Console.ReadLine());
+while (numberDayOnWeek < 1 || numberDayOnWeek > 7)
+{
+    Console.Write("Вы ошиблись!\nВведите порядковый номер дня недели: ");
+    numberDayOnWeek = Convert.ToInt32(Console.ReadLine());
+}    
+if(numberDayOnWeek == 1)
+    Console.Write("Этот день недели: Понедельник");
+if(numberDayOnWeek == 2)
+    Console.Write("Этот день недели: Вторник");
+if(numberDayOnWeek == 3)
+    Console.Write("Этот день недели: Среда");
+if(numberDayOnWeek == 4)
+    Console.Write("Этот день недели: Четверг");
+if(numberDayOnWeek == 5)
+    Console.Write("Этот день недели: Пятница");
+if(numberDayOnWeek == 6)
+    Console.Write("Этот день недели: Суббота");
+if(numberDayOnWeek == 7)
+    Console.Write("Этот день недели: Воскресенье"); 
