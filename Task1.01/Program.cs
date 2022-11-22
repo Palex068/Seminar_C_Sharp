bcfba70c4a863dda9c73aed21bc492f01c2769ce
Palex068 <palex068@gmail.com>
2022-11-22 12:07:41 +0300

    Change Task1.01

--- Task1.01/Program.cs
+++ Task1.01/Program.cs
@@ -1,2 +1,19 @@
 ﻿Console.Clear();
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+Console.Write("Введите первое число: ");
+// Способ ввода данных 1
+int number1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+// Способ ввода данных 2
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(number1 * 2);
+// Console.WriteLine(number2 * 3);
+
+Console.Write("Первое число больше чем второе? ");
+Console.WriteLine(number1 > number2);
+
+// Напишите программу, которая на вход 
+
