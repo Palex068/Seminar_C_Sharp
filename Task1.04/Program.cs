f9330fd4d1db28a766d20b13c2f68703b5a1ec30
Palex068 <palex068@gmail.com>
2022-11-22 17:20:17 +0300

    Add Task1.04 Вывод максимального из трех чисел

--- Task1.04/Program.cs
+++ Task1.04/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.Clear();
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Clear();
 
 Console.Write("Введите 1-е число: ");
 int Number1 = Convert.ToInt32(Console.ReadLine());
