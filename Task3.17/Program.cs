ac186a56205b36ccfc913a0e4e2d7dbf40c75a38
Palex068 <palex068@gmail.com>
2022-11-28 22:00:27 +0300

    Выдаёт номер четверти плоскости по координатам точки

---
+++ Task3.17/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Задача17
+    Напишите программу, которая принимает на вход координаты точки (X и Y),
+    причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+    2|1
+    3|4
+*/
+Console.Clear();
+
+Console.WriteLine("Введите координату Х:");         // Приглашение к вводу числа    
+
+double x = Convert.ToDouble(Console.ReadLine());    // Ввод числа
+
+Console.WriteLine("Введите координату Y:");         // Приглашение к вводу числа    
+
+double y = Convert.ToDouble(Console.ReadLine());    // Ввод числа
+
+if (x > 0 && y > 0)                                 
+    System.Console.WriteLine("I");
+
+else if (x < 0 && y > 0)                       
+    System.Console.WriteLine("II");
+
+else if (x < 0 && y < 0)                       
+    System.Console.WriteLine("III");
+
+else 
+    System.Console.WriteLine("IV");
+
+
+
