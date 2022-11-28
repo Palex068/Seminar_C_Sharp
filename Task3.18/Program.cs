4d88d74de0bc716eff18482c36ca45595eda37ff
Palex068 <palex068@gmail.com>
2022-11-28 22:03:52 +0300

    По номеру четверти показывает диапазон возможных коордитат

---
+++ Task3.18/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 18
+    Напишите программу, которая по заданному номеру четверти,
+    показывает диапазон возможных координат точек в этой четверти (x и y).
+*/
+Console.Clear();
+
+Console.WriteLine("Введите номер четверти:\n2 | 1\n-----\n3 | 4");          // Приглашение к вводу числа    
+int N = Convert.ToInt32(Console.ReadLine());                                // Ввод числа
+
+if (N == 1)                                 
+    System.Console.WriteLine("X > 0; Y > 0");
+        
+else if (N == 2)                       
+    System.Console.WriteLine("X < 0; Y > 0");
+
+else if (N == 3)                       
+    System.Console.WriteLine("X < 0; Y < 0");
+
+else 
+    System.Console.WriteLine("X > 0; Y < 0");
