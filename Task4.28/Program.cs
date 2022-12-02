0433395128032143115e0261aedc7b9d1365b35e
Palex068 <palex068@gmail.com>
2022-12-03 00:06:05 +0300

    Метод: Факториал числа

---
+++ Task4.28/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 28:
+    Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+    4 -> 24 
+    5 -> 120
+*/
+
+Console.Clear();                                    // Очистили консоль
+
+Console.Write("Введите число: ");                   // Приглашение к вводу числа
+
+int number = Convert.ToInt32(Console.ReadLine());   // Ввод
+
+int NumberFactorial(int number)                   // Объявили Метод
+{
+    int result = 1;                                 // Объявили переменную result = 0
+    
+    for (int i = 1; i < number + 1; i++)
+    {
+        result *=i;
+    }
+    return result;                                  // Возвращаем n!
+}
+
+Console.WriteLine();
+Console.WriteLine($"{number}! = {NumberFactorial(number)}");  // Применим Метод на числе и выведем результат
