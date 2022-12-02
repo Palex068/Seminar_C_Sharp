98e43c1ed4519b25c5bd2014866652e31675144b
Palex068 <palex068@gmail.com>
2022-12-02 21:46:02 +0300

    Вывод максимального и минимального из двух чисел добавлено ренение с помощью метода

--- Task1.02/Program.cs
+++ Task1.02/Program.cs
@@ -1,15 +1,30 @@
-﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿/* Задача 2:
+    Напишите программу, которая на вход принимает два числа и
+    выдаёт, какое число большее, а какое меньшее.
+*/
 
 Console.Clear();
 
 Console.Write("Введите 1-е число: ");
-int Number1 = Convert.ToInt32(Console.ReadLine());
+int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите 2-е число: ");
-int Number2 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
 
-if (Number2 > Number1)
-    Console.WriteLine($"Максимальное число равно: {Number2}\nМинимальное число равно: {Number1}");
+ // Решение через цикл
+
+if (number2 > number1)
+    Console.WriteLine($"Максимальное число равно: {number2}\nМинимальное число равно: {number1}");
 else
-    Console.WriteLine($"Максимальное число равно: {Number1}\nМинимальное число равно: {Number2}");
-    
+    Console.WriteLine($"Максимальное число равно: {number1}\nМинимальное число равно: {number2}");
+ 
+/* //Решение с помощью метода
+
+int NumbersMax(int n, int m)                            // Объявили Метод
+{
+    if (n > m)                                          // Цикл Если - сравнение
+        return n;
+    return m;
+}
+Console.WriteLine(NumbersMax (number1, number2));       // Вывод
+*/
 Console.WriteLine();
