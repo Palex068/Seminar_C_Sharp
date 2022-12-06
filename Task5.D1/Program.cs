454fe8ac333e3b4933166ea5c425cf0a36e4f5ce
Palex068 <palex068@gmail.com>
2022-12-06 20:29:44 +0300

    ДзD1 Гипотеза Гольдбаха - Решение через Решето Эратосфена

--- Task5.D1/Program.cs
+++ Task5.D1/Program.cs
@@ -20,40 +20,79 @@
         6 -> 3 3
         992 -> 73 919
 Алгоритм решения:
-    1. Создаем массив простых чисел от 2 до 997 =?
-        1.1 Создадим массив логических значений
-        1.2 Заполним массив значениями true начиная с 2(индекс) элемента
-            2 - это первое простое число
-        1.3 
+    1. Создаем метод создания массива простых чисел от 2 до введенного числа
+            1.1 Создадим массив логических значений
+            1.2 Заполним массив значениями true начиная с 2(индекс) элемента
+                2 - это первое простое число
+            1.3 Применим на массиве Алгоритм Решето Эратосфена https://ru.wikipedia.org/wiki/Решето_Эратосфена
+
     2. Объявим метод по поиску числа в массиве
     3. Запросим ввод числа
     4. Будем вычитать из введённого числа элементы массива
        с простыми числами по порядку возрастания и искать остаток в этом же массиве
     5. В случае нахождения остатка в массиве простых чисел получим ответ
+
+Алгоритмы для изучения https://habr.com/ru/post/526924/
 */ 
+
 
 Console.Clear();
 
-bool[] primeBoolArray = new bool[20];                                                 // Создаем логический массив))) 
+bool [] PrimeBoolArrayMethod(int numberForCheckGBSum)                           // Метод создания логического ряда простых чисел от 2 до
+{                                                                         // введённого числа
+    
+    bool[] primeBoolArray = new bool[numberForCheckGBSum];                // Создаем логический массив))) 
 
-Console.WriteLine($"\n[{string.Join("\t", primeBoolArray)}]");                            // Вывод массива
+    for (int i = 2; i < primeBoolArray.Length; i++)                       // Заполним массив значениями true
+    {
+        primeBoolArray[i] = true;
+    }
 
-for (int i = 2; i < primeBoolArray.Length; i++)                                           // Заполним массив значениями true
-{
-    primeBoolArray[i] = true;
+    for (int p = 2; p * p < primeBoolArray.Length; p++)                   // Применим Алгоритм Решето Эратосфена
+    {
+        if (primeBoolArray[p])                                            // Если элемент массива p = true, т.е. мы его ещё не исключили
+        {
+            for (int i = p * p; i < primeBoolArray.Length; i += p)        // Начиная от i равного p в квадрате с шагом p 
+            {
+                primeBoolArray[i] = false;                                // Исключим числа кратные p
+            }
+        }
+    }
+    return primeBoolArray;
 }
 
-Console.WriteLine($"\n[{string.Join("\t", primeBoolArray)}]");                            // Вывод массива
-
-for (int p = 2; p * p < primeBoolArray.Length; p++)                                       // Применим Алгоритм Решето Эратосфена
+// Console.WriteLine($"\n[{string.Join("\t", primeBoolArray)}]");     // Вывод массива
+/*
+for (int prime = 0; prime < primeBoolArray.Length; prime++)           // Вывод в консоль ряда простых чисел   
 {
-    if (primeBoolArray[p])
+    if(primeBoolArray[prime])
+        Console.Write($"{prime}\t");
+}
+*/
+
+void GoldbachsTest(int number)                                        // Метод нахождения пары чисел Гольдбаха с мин первым значением
+{
+    bool [] primeBoolArray = PrimeBoolArrayMethod(number);
+
+    for (int summandGBA = 0; summandGBA < number; summandGBA++)
     {
-        for (int i = p * p; i < primeBoolArray.Length; i += p)
+        if (primeBoolArray[summandGBA])
         {
-            primeBoolArray[i] = false;
-        }
+            if(primeBoolArray[number-summandGBA])
+            {
+                Console.WriteLine($"\n{number} -> {summandGBA} + {number-summandGBA}");
+                break;
+            }
+        }                                            
     }
 }
 
-Console.WriteLine($"\n[{string.Join("\t", primeBoolArray)}]");                            // Вывод массива
+Console.Write("Введите четное число от 4 до 998: ");
+int number = Convert.ToInt32(Console.ReadLine());                     // Ввод числа
+
+while (number < 4 || number > 998 || number % 2 ==1)
+{
+    Console.Write("Вы ошиблись!\nВведите четное число от 4 до 998: ");// Проверим на соответствие условию
+    number = Convert.ToInt32(Console.ReadLine());
+}
+GoldbachsTest(number);
