db06c2a367e1e7532f8be33fc3bd88e3afdf4cdf
Palex068 <palex068@gmail.com>
2022-12-09 10:47:49 +0300

    Преобразовывать десятичное число в двоичное

---
+++ Task6.42/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 42:
+Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+    45 -> 101101
+    3  -> 11
+    2  -> 10
+*/ 
+Console.Clear();
+
+Console.Write("Введите число n: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+string result = String.Empty;
+int n = number;
+
+while (n > 0)                                   // Вычисление ответа через остаток от деления на основание системы исчисления
+{
+    result = Convert.ToString(n % 2) + result;  // К полученному остатку приписываем предыдущие остатки
+    n /= 2;                                     // Само деление
+}
+
+Console.WriteLine(number + " -> " + result + " - Решение через вычисления" );
+
+Console.WriteLine($"{number} -> {Convert.ToString(number, 2)} - Решение через оператор \"Convert.ToString(n, 2)\"");
