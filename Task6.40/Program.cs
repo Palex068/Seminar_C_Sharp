146208f2231a3068097c31f34444fc1c5f468c9a
Palex068 <palex068@gmail.com>
2022-12-09 09:52:37 +0300

    Может ли существовать треугольник с сторонами такой длины

---
+++ Task6.40/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 40:
+    Напишите программу, которая 
+    принимает на вход три числа и 
+    проверяет, может ли существовать треугольник с сторонами такой длины.
+
+Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+*/
+
+Console.Write("Введите сторону А треугольника ABC: ");
+int sideA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите сторону B треугольника ABC: ");
+int sideB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите сторону C треугольника ABC: ");
+int sideC = Convert.ToInt32(Console.ReadLine());
+
+if (sideA + sideB > sideC)
+    if (sideC + sideB > sideA)
+        if(sideA + sideC > sideB)
+            Console.WriteLine("Такой треугольник существует");
+
+        else Console.WriteLine($"Такого треугольника не существует , т.к. A + C меньше либо равно B");
+
+    else Console.WriteLine($"Такого треугольника не существует , т.к. C + B меньше либо равно A");
+    
+else Console.WriteLine($"Такого треугольника не существует , т.к. A + B меньше либо равно C");
