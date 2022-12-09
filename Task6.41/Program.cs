88708afe0cd0ac19ff652df3db304ea6b2ccd1ee
Palex068 <palex068@gmail.com>
2022-12-09 10:29:15 +0300

    Дз41 Сколько чисел больше 0 ввёл пользователь

---
+++ Task6.41/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 41:
+    Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+    0, 7, 8, -2, -2 -> 2
+
+    1, -7, 567, 89, 223-> 3
+*/
+Console.Clear();
+
+Console.Write("Введите количество чисел М: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int countPositiveNumber = 0, number = 0;
+
+string resultList = "";
+
+for (int i = 1; i < m + 1; i++)
+{
+    Console.Write($"Введите {i} число: ");
+    number = Convert.ToInt32(Console.ReadLine());
+    
+    if(i == 1)
+        resultList += "[" + number.ToString() + ",";
+    
+    else if(i == m)
+        resultList += " " + number.ToString() + "]";
+    
+    else resultList += " " + number.ToString() + ",";
+    
+    if(number > 0)
+        countPositiveNumber++;    
+}
+
+Console.Write($"\n{resultList} -> {countPositiveNumber}");
