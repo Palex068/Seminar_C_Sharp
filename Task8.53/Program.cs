fb28d4461f25bb1bcd0392817062338da2749dae
Palex068 <palex068@gmail.com>
2022-12-15 22:18:02 +0300

    Меняет местами первую и последнюю строку массива

---
+++ Task8.53/Program.cs
@@ -0,0 +1,62 @@
+﻿/*Задача 53:
+    Задайте двумерный массив. 
+    Напишите программу, которая поменяет местами первую и последнюю строку массива.
+*/
+int[,] CreateMatrix() // Создаем двумерный массив (Пользователь вводит размер) // TODO написать проверку правильности ввода
+{
+    Console.Write("Введите размер матрицы (два числа через пробел): "); // TODO написать проверку правильности ввода
+    int[] arrayVolume = Console.ReadLine().Split(" ").Select(s => int.Parse(s)).ToArray();  // Исключили добавление переменных
+    int[,] matrix = new int[arrayVolume[0],arrayVolume[1]];
+    return matrix;
+}
+
+void InputPrintMatrix(int[,] matrix, int minNumber = (1), int maxNumber = 11 ) // Заполняем матрицу и выводим
+{
+    Console.WriteLine();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {   
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            
+            matrix[i,j] = new Random().Next(minNumber, maxNumber);
+            Console.Write($"{matrix[i,j]}\t");                                      // и выводим её
+        }        
+    Console.WriteLine();
+    }
+}
+
+void PrintMatrix(int[,] matrix)                     // Вывод матрицы
+{
+    Console.WriteLine();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {   
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i,j]}\t");                                      
+                
+    Console.WriteLine();
+    }
+}
+
+void ChangeMatrix(int[,] matrix)                    // Поменяет местами первую и последнюю строку массива.
+{
+    int temp;
+
+    for (int j = 0; j < matrix.GetLength(0); j++)
+    {   
+        temp = matrix[0,j];
+        matrix[0,j] = matrix[matrix.GetLength(0) - 1,j];
+        matrix[matrix.GetLength(0) - 1,j] = temp;
+    }
+}
+
+Console.Clear();
+
+int [,] matrix = CreateMatrix();
+
+InputPrintMatrix(matrix);
+
+ChangeMatrix(matrix);
+
+PrintMatrix(matrix);
