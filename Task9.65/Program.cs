4a6e94ecbdec4e7563bb3a93a533008553372963
Palex068 <palex068@gmail.com>
2022-12-19 23:21:17 +0300

    Рекурсия: выводим все натуральные числа в промежутке от M до N

---
+++ Task9.65/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 65:
+    Задайте значения M и N.
+    Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+    
+    M = 1; N = 5 -> "1, 2, 3, 4, 5"
+    M = 4; N = 8 -> "4, 5, 6, 7, 8"
+*/
+
+string rec(int m, int n)
+{
+    if (m == n)
+        return $"{m} ";
+    return rec(m, n - 1) + $"{n} ";
+}
+
+Console.Clear();
+
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(rec(m, n));
