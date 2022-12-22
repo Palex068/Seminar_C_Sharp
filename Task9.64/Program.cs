928df890861f07c1592218999622a7e2d44cd662
Palex068 <palex068@gmail.com>
2022-12-22 10:47:16 +0300

    Дз Выводит все четные натуральные числа в промежутке от N до 1

--- Task9.64/Program.cs
+++ Task9.64/Program.cs
@@ -9,16 +9,16 @@ string rec(int n)
 {
     if (n == 2)
         return "2 ";
-    return rec(n - 2) + $"{n} ";
+    if (n % 2  == 1)
+    {
+        n = n - 1;
+        return rec(n - 2) + $"{n} ";
+    }
+    else return rec(n - 2) + $"{n} ";
 }
 
 Console.Clear();
 Console.Write("Введите число N: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
-if (n % 2  == 1)
-{
-    n = n - 1;
-    Console.WriteLine(rec(n));
-}
-else Console.WriteLine(rec(n));
+Console.WriteLine(rec(n));
